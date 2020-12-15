cdb03e0b7a34f824cafcda41c30de5b81cd4cee8
robotdostopif <aron.cederlund@outlook.com>
2020-12-15 16:51:03 +0100

    Small indentation fix

--- Hotel.Server/Models/Request/RoomAvailabilityRequest.cs
+++ Hotel.Server/Models/Request/RoomAvailabilityRequest.cs
@@ -1,11 +1,10 @@
-
 using System;
 namespace Hotel.Server.Models.Request
 {
     public class RoomAvailabilityRequest
-{
-    public int Guests { get; set; }
-    public DateTime CheckInDate { get; set; }
-    public DateTime CheckOutDate { get; set; }
-}
+    {
+        public int Guests { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+    }
 }
