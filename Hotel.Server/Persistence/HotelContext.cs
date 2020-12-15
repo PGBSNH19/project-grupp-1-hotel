65c884205bceb4e23917eea93b1e4db32b5a9455
Pierre <pierre.nygard@gmail.com>
2020-12-15 17:00:24 +0100

    g1-63 finished

--- Hotel.Server/Persistence/HotelContext.cs
+++ Hotel.Server/Persistence/HotelContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Hotel.Server.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,8 @@ namespace Hotel.Server.Persistence
 {
     public class HotelContext : DbContext
     {
-        // todo: enable 
-        //public virtual DbSet<Room> Rooms { get; set; }  
-        //public virtual DbSet<Booking> Bookings { get; set; }  
+        public virtual DbSet<Room> Rooms { get; set; }
+        public virtual DbSet<Booking> Bookings { get; set; }
 
         public HotelContext(DbContextOptions options) : base(options)
         { }
@@ -19,156 +19,174 @@ namespace Hotel.Server.Persistence
         {
             base.OnModelCreating(builder);
 
-            // todo: enable 
-            //var room = builder.Entity<Room>();
-            //room.HasData(new
-            //{
-            //    Beds = 1,
-            //    DoubleBeds = 1,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = false,
-            //    Pets = false
-            //}, new
-            //{
-            //    Beds = 1,
-            //    DoubleBeds = 1,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = true,
-            //    Pets = false
-            //}, new
-            //{
-            //    Beds = 2,
-            //    DoubleBeds = 0,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = true,
-            //    Pets = false
-            //}, new
-            //{
-            //    Beds = 2,
-            //    DoubleBeds = 0,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = true,
-            //    Pets = true
-            //}, new
-            //{
-            //    Beds = 1,
-            //    DoubleBeds = 0,
-            //    IsCondo = false,
-            //    IsSuite = false,
-            //    Smoking = true,
-            //    Pets = false
-            //}, new
-            //{
-            //    Beds = 2,
-            //    DoubleBeds = 1,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = true,
-            //    Pets = false
-            //}, new
-            //{
-            //    Beds = 2,
-            //    DoubleBeds = 1,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = true,
-            //    Pets = false
-            //}, new
-            //{
-            //    Beds = 2,
-            //    DoubleBeds = 0,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = false,
-            //    Pets = false
-            //}, new
-            //{
-            //    Beds = 2,
-            //    DoubleBeds = 0,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = false,
-            //    Pets = false
-            //}, new
-            //{
-            //    Beds = 1,
-            //    DoubleBeds = 1,
-            //    IsCondo = false,
-            //    IsSuite = false,
-            //    Smoking = false,
-            //    Pets = true
-            //}, new
-            //{
-            //    Beds = 1,
-            //    DoubleBeds = 1,
-            //    IsCondo = true,
-            //    IsSuite = true,
-            //    Smoking = false,
-            //    Pets = true
-            //}, new
-            //{
-            //    Beds = 2,
-            //    DoubleBeds = 2,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = false,
-            //    Pets = true
-            //}, new
-            //{
-            //    Beds = 2,
-            //    DoubleBeds = 2,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = true,
-            //    Pets = false
-            //}, new
-            //{
-            //    Beds = 2,
-            //    DoubleBeds = 1,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = true,
-            //    Pets = false
-            //}, new
-            //{
-            //    Beds = 2,
-            //    DoubleBeds = 1,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = false,
-            //    Pets = true
-            //}, new
-            //{
-            //    Beds = 2,
-            //    DoubleBeds = 0,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = false,
-            //    Pets = true
-            //}, new
-            //{
-            //    Beds = 2,
-            //    DoubleBeds = 0,
-            //    IsCondo = true,
-            //    IsSuite = false,
-            //    Smoking = false,
-            //    Pets = false
-            //}, new
-            //{
-            //    Beds = 2,
-            //    DoubleBeds = 0,
-            //    IsCondo = false,
-            //    IsSuite = false,
-            //    Smoking = true,
-            //    Pets = false
-            //});
+            var room = builder.Entity<Room>();
+            room.HasKey(e => e.Id);
+            room.HasData(new
+            {
+                Id = 1,
+                Beds = 1,
+                DoubleBeds = 1,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = false,
+                Pets = false
+            }, new
+            {
+                Id = 2,
+                Beds = 1,
+                DoubleBeds = 1,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = true,
+                Pets = false
+            }, new
+            {
+                Id = 3,
+                Beds = 2,
+                DoubleBeds = 0,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = true,
+                Pets = false
+            }, new
+            {
+                Id = 4,
+                Beds = 2,
+                DoubleBeds = 0,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = true,
+                Pets = true
+            }, new
+            {
+                Id = 5,
+                Beds = 1,
+                DoubleBeds = 0,
+                IsCondo = false,
+                IsSuite = false,
+                Smoking = true,
+                Pets = false
+            }, new
+            {
+                Id = 6,
+                Beds = 2,
+                DoubleBeds = 1,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = true,
+                Pets = false
+            }, new
+            {
+                Id = 7,
+                Beds = 2,
+                DoubleBeds = 1,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = true,
+                Pets = false
+            }, new
+            {
+                Id = 8,
+                Beds = 2,
+                DoubleBeds = 0,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = false,
+                Pets = false
+            }, new
+            {
+                Id = 9,
+                Beds = 2,
+                DoubleBeds = 0,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = false,
+                Pets = false
+            }, new
+            {
+                Id = 10,
+                Beds = 1,
+                DoubleBeds = 1,
+                IsCondo = false,
+                IsSuite = false,
+                Smoking = false,
+                Pets = true
+            }, new
+            {
+                Id = 11,
+                Beds = 1,
+                DoubleBeds = 1,
+                IsCondo = true,
+                IsSuite = true,
+                Smoking = false,
+                Pets = true
+            }, new
+            {
+                Id = 12,
+                Beds = 2,
+                DoubleBeds = 2,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = false,
+                Pets = true
+            }, new
+            {
+                Id = 13,
+                Beds = 2,
+                DoubleBeds = 2,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = true,
+                Pets = false
+            }, new
+            {
+                Id = 14,
+                Beds = 2,
+                DoubleBeds = 1,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = true,
+                Pets = false
+            }, new
+            {
+                Id = 15,
+                Beds = 2,
+                DoubleBeds = 1,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = false,
+                Pets = true
+            }, new
+            {
+                Id = 16,
+                Beds = 2,
+                DoubleBeds = 0,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = false,
+                Pets = true
+            }, new
+            {
+                Id = 17,
+                Beds = 2,
+                DoubleBeds = 0,
+                IsCondo = true,
+                IsSuite = false,
+                Smoking = false,
+                Pets = false
+            }, new
+            {
+                Id = 18,
+                Beds = 2,
+                DoubleBeds = 0,
+                IsCondo = false,
+                IsSuite = false,
+                Smoking = true,
+                Pets = false
+            });
 
-            // no Booking seed
-            //var booking = builder.Entity<Booking>();
+            var booking = builder.Entity<Booking>();
+            booking.HasKey(e => e.Id);
         }
     }
 }
