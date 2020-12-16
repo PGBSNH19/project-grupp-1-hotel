c273bcf07ef3b81fef98d1321e64a6b22bfdea45
Pierre <pierre.nygard@gmail.com>
2020-12-16 14:14:51 +0100

    add beds to bookingrequest

---
+++ Hotel.Server/Services/BookingService.cs
@@ -0,0 +1,35 @@
+﻿using Hotel.Server.Models.Info;
+using Hotel.Server.Models.Request;
+using Hotel.Server.Services.Communication;
+using Hotel.Server.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel.Server.Services
+{
+    public class BookingService : IBookingService
+    {
+
+        private readonly IBookingRepository repo;
+
+        public BookingService(IBookingRepository repo) => this.repo = repo;
+
+        public Task<ServiceResponse<BookingInfo>> CreateAsync(BookingRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RoomInfo[]> GetAvailableRoomsAsync(RoomAvailabilityRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<BookingInfo> GetByBookingNumberAsync(string bookingNumber)
+        {
+            var result = await repo.GetByBookingNumberAsync(bookingNumber);
+            return result;
+        }
+    }
+}
---
+++ Hotel.Server/Services/Interfaces/IBookingService.cs
@@ -0,0 +1,17 @@
+﻿using Hotel.Server.Models.Info;
+using Hotel.Server.Models.Request;
+using Hotel.Server.Services.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel.Server.Services.Interfaces
+{
+    public interface IBookingService
+    {
+        Task<ServiceResponse<BookingInfo>> CreateAsync(BookingRequest request);
+        Task<RoomInfo[]> GetAvailableRoomsAsync(RoomAvailabilityRequest request);
+        Task<BookingInfo> GetByBookingNumberAsync(string bookingNumber);
+    }
+}
