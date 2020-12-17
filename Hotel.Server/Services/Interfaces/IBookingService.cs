bc7dd9c496918e70042d09fc61f2fd3e239f0910
Pierre <pierre.nygard@gmail.com>
2020-12-17 11:56:19 +0100

    fixed a bug

--- Hotel.Server/Services/Interfaces/IBookingService.cs
+++ Hotel.Server/Services/Interfaces/IBookingService.cs
@@ -1,6 +1,7 @@
 ﻿using Hotel.Server.Models.Info;
 using Hotel.Server.Models.Request;
 using Hotel.Server.Services.Communication;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Hotel.Server.Services.Interfaces
@@ -8,7 +9,7 @@ namespace Hotel.Server.Services.Interfaces
     public interface IBookingService
     {
         Task<ServiceResponse<BookingInfo>> CreateAsync(BookingRequest request);
-        Task<ServiceResponse<RoomInfo[]>> GetAvailableRoomTypesAsync(RoomAvailabilityRequest request);
+        Task<ServiceResponse<List<RoomInfo>>> GetAvailableRoomTypesAsync(RoomAvailabilityRequest request);
         Task<BookingInfo> GetByBookingNumberAsync(string bookingNumber);
     }
 }
