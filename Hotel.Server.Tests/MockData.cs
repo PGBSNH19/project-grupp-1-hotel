858ee9d2b27c512430e39012162e16e589d0a3e8
robotdostopif <aron.cederlund@outlook.com>
2020-12-17 11:57:06 +0100

    Added unit test for BookingService

--- Hotel.Server.Tests/MockData.cs
+++ Hotel.Server.Tests/MockData.cs
@@ -1,4 +1,5 @@
 ﻿using Hotel.Server.Models;
+using Hotel.Server.Models.Request;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,5 +20,14 @@ namespace Hotel.Server.Tests
                 new Room { Id = 2, Beds = 2, DoubleBeds = 1, IsCondo = true, IsSuite = false },
                 new Room { Id = 3, Beds = 2, DoubleBeds = 0, IsCondo = false, IsSuite = false },
             };
+
+        public static List<RoomAvailabilityRequest> MockRoomAvailabilityRequest => new List<RoomAvailabilityRequest>
+        {
+            new RoomAvailabilityRequest {CheckInDate = DateTime.Parse("Dec 17, 2020"), CheckOutDate = DateTime.Parse("Dec 18, 2020"), Guests = 1},
+            new RoomAvailabilityRequest {CheckInDate = DateTime.Parse("Dec 20, 2020"), CheckOutDate = DateTime.Parse("Dec 24, 2020"), Guests = 3},
+            new RoomAvailabilityRequest {CheckInDate = DateTime.Parse("Dec 20, 2020"), CheckOutDate = DateTime.Parse("Dec 24, 2020"), Guests = 2},
+            new RoomAvailabilityRequest {CheckInDate = DateTime.Parse("Dec 20, 2020"), CheckOutDate = DateTime.Parse("Dec 24, 2020"), Guests = 1},
+            new RoomAvailabilityRequest {CheckInDate = DateTime.Parse("Dec 22, 2020"), CheckOutDate = DateTime.Parse("Dec 26, 2020"), Guests = 3},
+        };
     }
 }
---
+++ Hotel.Server.Tests/ServiceTests/BookingServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Hotel.Server.Models;
+using Hotel.Server.Persistence;
+using Hotel.Server.Repositories;
+using Hotel.Server.Repositories.Interfaces;
+using Hotel.Server.Services;
+using Hotel.Server.Services.Interfaces;
+using Moq;
+using Moq.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hotel.Server.Tests.ServiceTests
+{
+    public class BookingServiceTests
+    {
+        private static BookingService GetRepoMockSetup(bool empty = false)
+        {
+            var ctx = new Mock<HotelContext>();
+            var rooms = MockData.MockRooms;
+            ctx.Setup(x => x.Rooms).ReturnsDbSet(empty ? new List<Room>() : rooms);
+            var bookingRepository = new BookingRepository(ctx.Object);
+            return new BookingService(bookingRepository);
+        }
+
+        [Fact]
+        public void GetAvailableRoomTypesAsync_IfNoRoomIsBooked_ReturnAllDistinctRoomTypes()
+        {
+            var service = GetRepoMockSetup();
+
+            var roomAvailablilityRequest = MockData.MockRoomAvailabilityRequest[0];
+
+            var result = service.GetAvailableRoomTypesAsync(roomAvailablilityRequest).Result.Entity.Length;
+            var expected = MockData.MockRooms.Distinct()
+                .Select(s => s.ToDto())
+                .ToArray().Length;
+
+            Assert.Equal(result, expected);
+
+        }
+    }
+}
