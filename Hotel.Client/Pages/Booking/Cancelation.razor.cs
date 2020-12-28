a162364de545cda90769910cd557c007b9940a1a
MirkoPralica <56869159+MirkoPralica@users.noreply.github.com>
2020-12-28 10:32:52 +0100

    Just saving on the issue

--- Hotel.Client/Pages/Booking/Cancelation.razor.cs
+++ Hotel.Client/Pages/Booking/Cancelation.razor.cs
@@ -1,27 +1,27 @@
-﻿//using Hotel.Shared;
-//using Microsoft.AspNetCore.Components;
-//using Microsoft.Extensions.Configuration;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Net.Http;
-//using System.Net.Http.Json;
-//using System.Threading.Tasks;
+﻿using Hotel.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
-//namespace Hotel.Client.Pages.Booking
-//{
-//    public partial class Cancelation
-//    {
-//        [Inject] HttpClient Http { get; set; }
-//        [Inject] IConfiguration Configuration { get; set; }
+namespace Hotel.Client.Pages.Booking
+{
+    public partial class Cancelation
+    {
+        [Inject] HttpClient Http { get; set; }
+        [Inject] IConfiguration Configuration { get; set; }
 
-//        [Parameter]
-//        public int bookingNumber { get; set; }
-//        public BookingInfo bookinginfo { get; set; }
-//        protected override async Task OnInitializedAsync()
-//        {
-//            bookinginfo = await Http.GetFromJsonAsync<BookingInfo>($"{Configuration["BaseApiUrl"]}api/v1.0/booking/{bookingNumber}");
-//        }
+        [Parameter]
+        public string bookingNumber { get; set; }
+        public BookingInfo bookinginfo { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            bookinginfo = await Http.GetFromJsonAsync<BookingInfo>($"{Configuration["BaseApiUrl"]}api/v1.0/booking/{bookingNumber}");
+        }
 
-//    }
-//}
+    }
+}
