f732616400957d23b1554de2f55758b73de17e21
robotdostopif <aron.cederlund@outlook.com>
2020-12-28 16:16:31 +0100

    Now CancelBookingForm redirects to cancel booking page

--- Hotel.Client/Shared/CancelBookingForm.razor.cs
+++ Hotel.Client/Shared/CancelBookingForm.razor.cs
@@ -13,24 +13,9 @@ namespace Hotel.Client.Shared
     {
         public string BookingNumber { get; set; }
         [Parameter] public EventCallback EventCallback { get; set; }
-        [Inject] AppState AppState { get; set; }
-        [Inject] HttpClient Http { get; set; }
-        [Inject] IConfiguration Configuration { get; set; }
-        [Inject] NavigationManager Nav { get; set; }
-        private BookingInfo BookingInfo { get; set; }
-        async Task GetBooking()
+        void GetBooking(string bookingNumber)
         {
-                BookingInfo = await Http.GetFromJsonAsync<BookingInfo>
-                     ($"{Configuration["BaseApiUrl"]}api/v1.0/booking/{BookingNumber}");
-                if (BookingInfo != null)
-                {
-                    AppState.SetCancelBooking(BookingInfo);
-                    Nav.NavigateTo($"cancel/{BookingNumber}");
-                }
-                else
-                {
-                    // todo: toast notification
-                }
+            
         }
     }
 }
