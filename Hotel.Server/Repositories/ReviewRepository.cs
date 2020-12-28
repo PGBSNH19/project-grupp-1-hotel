40aa4ac550dde8b1f220ab9918d02f64ca8abcaa
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-12-28 11:33:24 +0100

    commit before merge

---
+++ Hotel.Server/Repositories/ReviewRepository.cs
@@ -0,0 +1,16 @@
+﻿using Hotel.Server.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel.Server.Repositories
+{
+    public class ReviewRepository : BaseRepository
+    {
+        public ReviewRepository(HotelContext ctx) : base(ctx)
+        {
+
+        }
+    }
+}
---
+++ Hotel.Server/Services/Interfaces/IReviewService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel.Server.Services.Interfaces
+{
+    public interface IReviewService
+    {
+        Task GetRandomReviewsAsync();
+        Task GetAverageAsync();
+        Task CreateReviewAsync();
+    }
+}
