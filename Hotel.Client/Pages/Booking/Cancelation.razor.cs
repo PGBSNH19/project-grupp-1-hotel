0b3d0089ab59c52695147c2d88eb6a333900b901
MirkoPralica <56869159+MirkoPralica@users.noreply.github.com>
2020-12-28 09:40:29 +0100

    Started working on the issue, just saving changes

---
+++ Hotel.Client/Pages/Booking/Cancelation.razor.cs
@@ -0,0 +1,27 @@
+﻿//using Hotel.Shared;
+//using Microsoft.AspNetCore.Components;
+//using Microsoft.Extensions.Configuration;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Net.Http.Json;
+//using System.Threading.Tasks;
+
+//namespace Hotel.Client.Pages.Booking
+//{
+//    public partial class Cancelation
+//    {
+//        [Inject] HttpClient Http { get; set; }
+//        [Inject] IConfiguration Configuration { get; set; }
+
+//        [Parameter]
+//        public int bookingNumber { get; set; }
+//        public BookingInfo bookinginfo { get; set; }
+//        protected override async Task OnInitializedAsync()
+//        {
+//            bookinginfo = await Http.GetFromJsonAsync<BookingInfo>($"{Configuration["BaseApiUrl"]}api/v1.0/booking/{bookingNumber}");
+//        }
+
+//    }
+//}
