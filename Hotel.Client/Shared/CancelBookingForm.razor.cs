799c6da580048c69681b3c936104b342808f95b9
robotdostopif <aron.cederlund@outlook.com>
2020-12-29 09:35:17 +0100

    Now the design for CancelBookingForm looks ok

--- Hotel.Client/Shared/CancelBookingForm.razor.cs
+++ Hotel.Client/Shared/CancelBookingForm.razor.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -13,9 +14,15 @@ namespace Hotel.Client.Shared
     {
         public string BookingNumber { get; set; }
         [Parameter] public EventCallback EventCallback { get; set; }
-        void GetBooking(string bookingNumber)
+
+        [Parameter] public EventCallback<string> OnClick { get; set; }
+
+        async Task OnClickInternal()
         {
-            
+            if (OnClick.HasDelegate)
+            {
+                await OnClick.InvokeAsync(BookingNumber);
+            }
         }
     }
 }
