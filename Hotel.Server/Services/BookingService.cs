22525d66500c52774ea2b9b3d913700dd1fb3d30
Fredrika Eriksson <56867257+erifredrika@users.noreply.github.com>
2020-12-29 12:05:37 +0100

    Mail works, next step is implement secret (not in issue but can be nice)

--- Hotel.Server/Services/BookingService.cs
+++ Hotel.Server/Services/BookingService.cs
@@ -4,10 +4,13 @@ using Hotel.Server.Services.Communication;
 using Hotel.Server.Services.Interfaces;
 using Hotel.Shared;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Hotel.Server.Services
@@ -47,6 +50,51 @@ namespace Hotel.Server.Services
             {
                 await repo.AddAsync(entity);
                 await repo.Complete();
+                Console.WriteLine(entity.Email);
+                try
+                {
+                    var gmailPass = "Qa8e6aA!Nr$Hi2";
+                    MailMessage message = new MailMessage();
+                    message.To.Add(entity.Email);
+
+                    message.From = new MailAddress("hotellgruppett@gmail.com", "Hotell Group");
+                    message.Subject = $"Booking Details for {entity.FirstName} {entity.LastName}";
+                    message.Body = $"" +
+                        $"<div style='background: #eee; margin: 20px; padding: 20px;'>" +
+                        $"<center><h1>Hotell </h1><br>" +
+                        $"<h4> We look forward to your stay.</h4>" +
+                        $"<font> Check in is between 12:00 - 4:00 PM.<br>" +
+                        $"Checkout before 12:00 PM on your last day.<br>" +
+                        $"You have booked a room between the dates {entity.CheckInDate}" +
+                        $"- {entity.CheckOutDate}.<br></font></center><br><br>" +
+                        $"<font><b> Your booking details:</b><br><br>" +
+                        $"Number of guests - {entity.Guests}<br>" +
+                        $"Check In - {entity.CheckInDate}<br>" +
+                        $"Check Out - {entity.CheckOutDate}</font></div>";
+                    message.IsBodyHtml = true;
+                    using (var smtp = new SmtpClient())
+                    {
+                        var credential = new NetworkCredential
+                        {
+                            UserName = "hotellgruppett@gmail.com",
+                            Password = gmailPass  
+                        };
+                        smtp.Credentials = credential;
+                        smtp.Host = "smtp.gmail.com";
+                        smtp.Port = 587;
+                        smtp.EnableSsl = true;
+                        smtp.Send(message);
+                    }
+                }
+                catch (Exception e)
+                {
+                    string msg = "Mail cannot be sent";
+                    msg += e.Message;
+                    Log.Debug("Error: Inside catch block of Mail sending");
+                    Log.Error("Error msg:" + e);
+                    Log.Error("Stack trace:" + e.StackTrace);
+                }
+              
             }
             catch (Exception ex)
             {
