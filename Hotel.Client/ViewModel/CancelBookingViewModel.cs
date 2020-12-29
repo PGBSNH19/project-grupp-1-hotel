47b50bd5b0209ff15ddfb10c01c192df197c0796
robotdostopif <aron.cederlund@outlook.com>
2020-12-29 13:32:22 +0100

    Added ViewModel for CancelBooking and edited css for nav

---
+++ Hotel.Client/ViewModel/CancelBookingViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Client.ViewModel
+{
+    public class CancelBookingViewModel
+    {
+        [Required]
+        [MaxLength(64, ErrorMessage = "Bookingnumber is too long"), MinLength(24, ErrorMessage = "Bookingnumber is too short")]
+        public string BookingNumber { get; set; }
+    }
+}
---
+++ Hotel.Shared/CancelBookingRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Client.Shared
+{
+    public class CancelBookingRequest
+    {
+        [Required]
+        [MaxLength(64), MinLength(24)]
+        public string BookingNumber { get; set; }
+    }
+}
