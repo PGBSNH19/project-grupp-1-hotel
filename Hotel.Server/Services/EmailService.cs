f25a58f2a813129e6189c995186e8bc12a1544d4
robotdostopif <aron.cederlund@outlook.com>
2021-01-04 15:55:29 +0100

    Edited e-mail content

--- Hotel.Server/Services/EmailService.cs
+++ Hotel.Server/Services/EmailService.cs
@@ -9,6 +9,7 @@ using Hotel.Server.Models;
 using System.Globalization;
 using System.Net;
 using Hotel.Server.Services.Interfaces;
+using Hotel.Shared;
 
 namespace Hotel.Server.Services
 {
@@ -21,6 +22,19 @@ namespace Hotel.Server.Services
             _configuration = configuration;
         }
 
+        private string RoomTypeInfo(Booking entity)
+        {
+            return (entity.Room.Beds, entity.Room.DoubleBeds) switch
+            {
+                (1, 0) => "Single bed",
+                (1, 1) => "Double bed",
+                (2, 2) => "Master suite",
+                (2, 1) => "Triple bed",
+                (2, 0) => "Twin bed",
+                _ => string.Empty
+            };
+        }
+
         public void SendMessage(Booking entity)
         {
             var emailUsername = _configuration.GetSection("EmailService").GetSection("EmailUserName").Value;
@@ -28,7 +42,7 @@ namespace Hotel.Server.Services
             var emailSmtpHost = _configuration.GetSection("EmailService").GetSection("EmailSmtpHost").Value;
             var emailSmtpPort = _configuration.GetSection("EmailService").GetSection("EmailSmtpPort").Value;
             var mailbody = CreateMessageContent(entity);
-            var message = CreateMailMessage(entity, mailbody);
+            var message = CreateMailMessage(entity, mailbody, emailUsername, "Hotell Group");
 
             using (var smtp = new SmtpClient())
             {
@@ -45,11 +59,11 @@ namespace Hotel.Server.Services
             }
         }
 
-        private MailMessage CreateMailMessage(Booking entity, StringBuilder mailbody)
+        private MailMessage CreateMailMessage(Booking entity, StringBuilder mailbody, string emailUsername, string displayName)
         {
             MailMessage message = new MailMessage();
             message.To.Add(entity.Email);
-            message.From = new MailAddress("hotellgruppett@gmail.com", "Hotell Group");
+            message.From = new MailAddress(emailUsername, displayName);
             message.Subject = $"Booking Details for {entity.FirstName} {entity.LastName}";
             message.Body = mailbody.ToString();
             message.IsBodyHtml = true;
@@ -62,24 +76,37 @@ namespace Hotel.Server.Services
 
             string checkin = entity.CheckInDate.ToString("dd/M/yyyy", CultureInfo.InvariantCulture);
             string checkout = entity.CheckOutDate.ToString("dd/M/yyyy", CultureInfo.InvariantCulture);
+            string roomType = RoomTypeInfo(entity);
+
             string breakfastIncluded = entity.Breakfast? $"Breakfast - Included<br>":"";
             string spaIncluded = entity.SpaAccess ? "Spa Access - Included<br>" : "";
+
+            string created = entity.Created.ToString("dd/M/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
 
             mailbody.Append($"" +
                             $"<div style='background: #eee; margin: 20px; padding: 20px;'>" +
                             $"<center><h1>Hotell </h1><br>" +
-                            $"<h4> We look forward to your stay.</h4>" +
-                            $"<font> Check in is between 12:00 - 4:00 PM.<br>" +
-                            $"Checkout before 12:00 PM on your last day.<br>" +
+                            $"<h4> We look forward to your stay {entity.FirstName}.</h4>" +
+                            $"<font> Check in is between 12:00 - 16:00.<br>" +
+                            $"Checkout before 12:00 on your last day.<br>" +
                             $"You have booked a room between the dates {checkin}" +
                             $"- {checkout}.<br></font><br><br>" +
                             $"<b>Booking No<b>: {entity.BookingNumber} <br><br></center>" +
                             $"<font><b> Your booking details:</b><br><br>" +
+                            $"First name - {entity.FirstName}<br>" +
+                            $"Last name - {entity.LastName}<br>" +
+                            $"Phone number - {entity.PhoneNumber}<br>" +
+                            $"Address - {entity.Address}<br>" +
+                            $"E-mail - {entity.Email}<br>" +
                             $"Number of guests - {entity.Guests}<br>" +
+                            $"Room type - {roomType}<br>" +
                             $"Check In - {checkin}<br>" +
                             $"Check Out - {checkout}<br>" +
                             $"{spaIncluded}" +
-                            $"{breakfastIncluded}</font></div>");
+                            $"{breakfastIncluded}" +
+                            $"Created at - {created}</font></div>"
+                            );
+                            
 
             return mailbody;
         }
