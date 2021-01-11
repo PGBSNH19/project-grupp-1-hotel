86a1ad5c695c5d39ac76c50be593ad1757dce68a
Pierre <pierre.nygard@gmail.com>
2021-01-11 13:43:33 +0100

    nuget with email regex

--- Hotel.Shared/BookingRequest.cs
+++ Hotel.Shared/BookingRequest.cs
@@ -15,23 +15,23 @@ namespace Hotel.Shared
         [DataType(DataType.Date)]
         public DateTime CheckOutDate { get; set; }
         [Required]
-        [MaxLength(20), MinLength(2)]
+        [MaxLength(20, ErrorMessage = "Must have a length between 2 and 20."), MinLength(2, ErrorMessage = "Must have a length between 2 and 20.")]
         public string FirstName { get; set; }
         [Required]
-        [MaxLength(20), MinLength(2)]
+        [MaxLength(20, ErrorMessage = "Must have a length between 2 and 20."), MinLength(2, ErrorMessage = "Must have a length between 2 and 20.")]
         public string LastName { get; set; }
         [Required(ErrorMessage = "The email address is required")]
-        [EmailAddress(ErrorMessage = "Invalid Email Address")]
-        [MaxLength(25), MinLength(6)]
+        [RegularExpression(@"^([a-zA-Z]{2,20}).(@[a-zA-Z.]{2,20}).([a-zA-Z]{2,6}$)", ErrorMessage = "Invalid Email Address")]
+        [MaxLength(50, ErrorMessage = "Must have a length between 7 and 50."), MinLength(7, ErrorMessage = "Must have a length between 7 and 50.")]
         public string Email { get; set; }
         [Required]
         [RegularExpression(@"^(\+)?([0-9]{7,15})$", ErrorMessage = "Phone number not valid. Must have length between 7 and 15 and may only contain digits and plus sign.")]
         public string PhoneNumber { get; set; }
         [Required]
-        [MaxLength(30), MinLength(4)]
+        [MaxLength(30, ErrorMessage = "Must have a length between 4 and 30."), MinLength(4, ErrorMessage = "Must have a length between 4 and 30.")]
         public string Address { get; set; }
         [Required]
-        [Range(1, 4)]
+        [Range(1, 4, ErrorMessage = "Allowed number of guests are between 1 and 4.")]
         public int Guests { get; set; }
         public bool Breakfast { get; set; } = false;
         public bool SpaAccess { get; set; } = false;
